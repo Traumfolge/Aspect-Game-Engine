0d4946b4c50552054dd65cd1f48648d5a017a5c5
Max <83046445+Traumfolge@users.noreply.github.com>
2021-10-04 16:37:44 -0400

    Basic UI

---
+++ Aspect/AspectEditor/GameProject/NewProject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspectEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "NewProject";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+    }
+
+}
